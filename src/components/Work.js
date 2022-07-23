ae442b91b1715e6bb99e074485089fb3838e6530
Akshit Goyal <akshit2392@gmail.com>
2022-07-23 23:32:29 +0530

    changes 2

--- src/components/Work.js
+++ src/components/Work.js
@@ -16,7 +16,7 @@ function Work() {
         <div className="aeroDesign wc"> <b><u>Aero-Design Team</u></b> is the most experienced team that has performed well at many national level competitions and now
         they have been working more advanced and innovative designs such as box-wing aircraft.</div>
         <div className="UAVDrone wc"><b><u>UAV/Drone Design Team</u></b> has been built in 2021 and they are already successful at a few national competitions. The major focus of this team is to work on latest UAV design and control systems along with applications in different industries such as agriculture. They are equipped with ample technical skills like electronics, flight control systems, Computer Vision, and Artificial Intelligence.</div>        
-        <div className="rocket wc"><b><u>Rocket Design Team</u></b> is completely research-based as of now and they have simulated numerous Stage-I model rockets.</div>
+        <div className="rocketTeam wc"><b><u>Rocket Design Team</u></b> is completely research-based as of now and they have simulated numerous Stage-I model rockets.</div>
       </div>
       <div className='workImages'>
         <div className="workImage">
